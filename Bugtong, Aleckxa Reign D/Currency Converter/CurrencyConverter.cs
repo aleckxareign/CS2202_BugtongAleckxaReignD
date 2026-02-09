7d4c605e1305e772d38e0b2743e2fed55fa0c909
REI <palmcaeries@gmail.com>
2026-02-09 10:27:54 +0800

    Add files via upload

---
+++ Bugtong, Aleckxa Reign D/Currency Converter/CurrencyConverter.cs
@@ -0,0 +1,13 @@
+﻿double amount;
+double exchangeRate;
+double convertedAmount;
+
+Console.Write("Enter amount in PHP: ");
+amount = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter exchange rate from PHP to USD: ");
+exchangeRate = Convert.ToDouble(Console.ReadLine());
+
+convertedAmount = amount * exchangeRate;
+
+Console.WriteLine($"Amount in USD: {convertedAmount:F2}");
---
+++ Bugtong, Aleckxa Reign D/Interest Calculator/InterestCalculator.cs
@@ -0,0 +1,17 @@
+﻿double principal;
+double rate;
+double time;
+double simpleInterest;
+
+Console.Write("Enter principal amount: ");
+principal = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter rate of interest: ");
+rate = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter time in years: ");
+time = Convert.ToDouble(Console.ReadLine());
+
+simpleInterest = (principal * rate * time) / 100;
+
+Console.WriteLine($"Simple Interest: {simpleInterest:F2}");
