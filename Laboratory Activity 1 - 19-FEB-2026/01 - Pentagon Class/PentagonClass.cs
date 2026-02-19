fcdd22cf39d77036609b48c4e48ae64476b377b9
REI <palmcaeries@gmail.com>
2026-02-19 08:44:14 +0800

    Add files via upload

---
+++ Laboratory Activity 1 - 19-FEB-2026/01 - Pentagon Class/PentagonClass.cs
@@ -0,0 +1,46 @@
+﻿class Pentagon
+{
+    private double side;
+
+    public Pentagon()
+    {
+        side = 0;
+    }
+
+    public Pentagon(double s)
+    {
+        side = s;
+    }
+
+    public double GetPerimeter()
+    {
+        return 5 * side;
+    }
+
+    public double GetArea()
+    {
+        return (Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5))) * side * side) / 4;
+    }
+
+    public void Display(string name)
+    {
+        Console.WriteLine(name);
+        Console.WriteLine("------------------------");
+        Console.WriteLine($"Side length: " + side);
+        Console.WriteLine($"Perimeter: " + GetPerimeter().ToString("F3"));
+        Console.WriteLine($"Area: " + GetArea().ToString("F3"));
+        Console.WriteLine();
+    }
+}
+
+    class PentagonClass
+    {
+        public static void Main(String[] args)
+        {
+            Pentagon p1 = new Pentagon();
+            p1.Display("Pentagon p1");
+
+            Pentagon p2 = new Pentagon(5);
+            p2.Display("Pentagon p2");
+        }
+    }
